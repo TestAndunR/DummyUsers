6531c36f74f97c69bba0ff56ef1cfacbaa9c7482
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-09 15:37:33 +0530

    updsate lat and lon

---
+++ DummyUsers/firebasetest.js
@@ -0,0 +1,15 @@
+
+let Firebase = require('firebase');
+class Firebase{
+	constructor(){
+		let config = {
+			apiKey: "AIzaSyDMF17Pu7_5gLRYX_Ff_dbA8Ak5_RONIQA",
+			authDomain: "testusercreate-89b32.firebaseapp.com",
+			databaseURL: "https://testusercreate-89b32.firebaseio.com",
+			projectId: "testusercreate-89b32",
+			storageBucket: "testusercreate-89b32.appspot.com",
+			messagingSenderId: "157406510033"
+		}
+	}
+	
+}
--- DummyUsers/lambda.js
+++ DummyUsers/lambda.js
@@ -1,12 +1,13 @@
+import firebase from "firebase";
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 	let email = "andun@adroitlogic.com";
 	// let long = 7.8731;
-	let changeCoord = (Math.random() * (0.120 - 0.0200) + 0.0200).toFixed(4);
+	changeCoord = (Math.random() * (0.120 - 0.0200) + 0.0200).toFixed(4);
 	console.log(changeCoord);
-	let long = 7.8731 + changeCoord;
-	let latitude = 80.7718 + changeCoord;
+	let long = 7.8731 + parseFloat(changeCoord);
+	let latitude = 80.7718 + parseFloat(changeCoord);
 	console.log(long,latitude);
 	password = "12345678"
 	ddb.put({
@@ -27,6 +28,33 @@ exports.handler = function (event, context, callback) {
 			console.log("Success");
 		}
 	});
+	class FirebaseService{
+		constructor(){
+			let config = {
+				apiKey: "AIzaSyDMF17Pu7_5gLRYX_Ff_dbA8Ak5_RONIQA",
+				authDomain: "testusercreate-89b32.firebaseapp.com",
+				databaseURL: "https://testusercreate-89b32.firebaseio.com",
+				projectId: "testusercreate-89b32",
+				storageBucket: "testusercreate-89b32.appspot.com",
+				messagingSenderId: "157406510033"
+			}
+			if (!firebase.apps.length) {
+            firebase.initializeApp(config);
+			console.log("Success");
+        }
+
+        //TODO configure proper authentication
+        firebase.auth().signInWithEmailAndPassword('andun@adroitlogic.com', 'Andun!12345').catch(error => {
+            console.log(error);
+        });
+        // Get a reference to the database service
+        this.database = firebase.database();
+		}
+
+
+		
+	}
 
 	callback(null, 'Successfully executed');
+	
 }
