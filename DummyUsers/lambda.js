f23cf854ec1c7652668f2829bb718dc265574540
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-05-10 10:18:12 +0530

    remove trigger

--- DummyUsers/lambda.js
+++ DummyUsers/lambda.js
@@ -34,12 +34,12 @@ exports.handler = function (event, context, callback) {
 	// class FirebaseService{
 	// 	constructor(){
 	// 		let config = {
-	// 			apiKey: "AIzaSyDMF17Pu7_5gLRYX_Ff_dbA8Ak5_RONIQA",
-	// 			authDomain: "testusercreate-89b32.firebaseapp.com",
-	// 			databaseURL: "https://testusercreate-89b32.firebaseio.com",
-	// 			projectId: "testusercreate-89b32",
-	// 			storageBucket: "testusercreate-89b32.appspot.com",
-	// 			messagingSenderId: "157406510033"
+				// apiKey: "AIzaSyDMF17Pu7_5gLRYX_Ff_dbA8Ak5_RONIQA",
+				// authDomain: "testusercreate-89b32.firebaseapp.com",
+				// databaseURL: "https://testusercreate-89b32.firebaseio.com",
+				// projectId: "testusercreate-89b32",
+				// storageBucket: "testusercreate-89b32.appspot.com",
+				// messagingSenderId: "157406510033"
 	// 		}
 	// 		if (!firebase.apps.length) {
     //         firebase.initializeApp(config);
